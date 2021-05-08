60e1d78232d8cea96ab491360b88785bf5dd27f1
SrJGoulart <jfdosscgoulart@gmail.com>
2021-05-07 22:44:49 -0300

    Create produtos.js

---
+++ Projeto Final/js/produtos.js
@@ -0,0 +1,20 @@
+
+function getLsProdutos() {
+    return lsProdutos = [
+        { "cod": "01", "valor": 10, "descricao": "pão, salsicha Perdigão, presunto, mussarela, milho, tomate, batata palha e maionese.", "grupo": "Salsicha na Chapa", "tipo": "HOTDOG", "qt": 0 },
+        { "cod": "02", "valor": 10, "descricao": "pão, salsicha perdigão, presunto, mussarela, milho, tomate, bacon e maionese.", "grupo": "Salsicha na Chapa", "tipo": "HOTDOG", "qt": 0 },
+        { "cod": "03", "valor": 12, "descricao": "pão, filé de frango, bacon, mussarela, maionese, milho, tomate, alface e cebola roxa.", "grupo": "Frango na Chapa", "tipo": "HOTDOG", "qt": 0 },
+        { "cod": "04", "valor": 17, "descricao": "Bomba de frango, pão, 2 filé de frango, 2 queijos mussarela, salsicha Perdigão, calabresa, bacon, maionese, milho, tomate.", "grupo": "Frango na Chapa", "tipo": "HOTDOG", "qt": 0 },
+        { "cod": "05", "valor": 13, "descricao": "pão, filé minon, bacon, mussarela, maionese, milho, tomate, alface e cebola roxa.", "grupo": "Filé Mignon na Chapa", "tipo": "HOTDOG", "qt": 0 },
+        { "cod": "06", "valor": 10, "descricao": "pão, salsicha Perdigão, presunto, mussarela, milho, tomate, batata palha e maionese.", "grupo": "Salsicha na Chapa", "tipo": "HOTDOG", "qt": 0 },
+        { "cod": "07", "valor": 10, "descricao": "pão, salsicha perdigão, presunto, mussarela, milho, tomate, bacon e maionese.", "grupo": "Salsicha na Chapa", "tipo": "HOTDOG", "qt": 0 },
+        { "cod": "08", "valor": 12, "descricao": "pão, filé de frango, bacon, mussarela, maionese, milho, tomate, alface e cebola roxa.", "grupo": "Frango na Chapa", "tipo": "HOTDOG", "qt": 0 },
+        { "cod": "09", "valor": 17, "descricao": "Bomba de frango, pão, 2 filé de frango, 2 queijos mussarela, salsicha Perdigão, calabresa, bacon, maionese, milho, tomate.", "grupo": "Frango na Chapa", "tipo": "HOTDOG", "qt": 0 },
+        { "cod": "10", "valor": 13, "descricao": "pão, filé minon, bacon, mussarela, maionese, milho, tomate, alface e cebola roxa.", "grupo": "Filé Mignon na Chapa", "tipo": "HOTDOG", "qt": 0 },
+        { "cod": "11", "valor": 13, "descricao": "pão, acém, bacon, mussarela, maionese, milho, tomate, alface e cebola roxa.", "grupo": "Acém na Chapa", "tipo": "HOTDOG", "qt": 0 },
+        { "cod": "", "valor": 4.9, "descricao": "Abacaxi", "grupo": "Sucos de 500ml", "tipo": "POLPAS", "qt": 0 },
+        { "cod": "", "valor": 4.9, "descricao": "Acerola", "grupo": "Sucos de 500ml", "tipo": "POLPAS", "qt": 0 },
+        { "cod": "", "valor": 4.9, "descricao": "Caju", "grupo": "Sucos de 500ml", "tipo": "POLPAS", "qt": 0 },
+        { "cod": "", "valor": 4.9, "descricao": "Goiaba", "grupo": "Sucos de 500ml", "tipo": "POLPAS", "qt": 0 },
+        { "cod": "", "valor": 4.9, "descricao": "Manga", "grupo": "Sucos de 500ml", "tipo": "POLPAS", "qt": 0 },
+    ]}
